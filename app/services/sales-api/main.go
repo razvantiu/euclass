f5d21e07d268ff8352fa97b39555083cb5832dec
William Kennedy <bill@ardanlabs.com>
2022-02-07 11:57:35 -0500

    day1 push13

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -6,6 +6,8 @@ import (
 	"os/signal"
 	"runtime"
 	"syscall"
+
+	"github.com/emadolsky/automaxprocs/maxprocs"
 )
 
 // build is the git version of this program. It is set using build flags in the makefile.
@@ -15,6 +17,12 @@ func main() {
 	l := log.New(os.Stdout, "TEST", log.Lshortfile|log.Ldate|log.Ltime)
 
 	l.Println("Starting Service")
+
+	// go get github.com/emadolsky/automaxprocs@master
+	if _, err := maxprocs.Set(); err != nil {
+		l.Println("GOMAXPROCS:", err)
+
+	}
 	l.Println("GOMAXPROCS:", runtime.GOMAXPROCS(0))
 
 	defer l.Println("Stopped Service")
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ardanlabs/service
 
 go 1.17
+
+require github.com/emadolsky/automaxprocs v1.4.1-0.20220104203543-349281033d17 // indirect
