b471123033c9bf120b3df6c30ac925ab57415719
William Kennedy <bill@ardanlabs.com>
2022-02-08 09:04:29 -0500

    day2 push1

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,31 +1,26 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/signal"
-	"runtime"
 	"syscall"
 
-	"github.com/emadolsky/automaxprocs/maxprocs"
+	"github.com/ardanlabs/service/foundation/logger"
 )
 
 // build is the git version of this program. It is set using build flags in the makefile.
 var build = "develop"
 
 func main() {
-	l := log.New(os.Stdout, "TEST", log.Lshortfile|log.Ldate|log.Ltime)
 
-	l.Println("Starting Service")
-
-	// go get github.com/emadolsky/automaxprocs@master
-	if _, err := maxprocs.Set(); err != nil {
-		l.Println("GOMAXPROCS:", err)
-
+	// Construct the application logger.
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	l.Println("GOMAXPROCS:", runtime.GOMAXPROCS(0))
-
-	defer l.Println("Stopped Service")
+	defer log.Sync()
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/ardanlabs/service
 
 go 1.17
 
-require github.com/emadolsky/automaxprocs v1.4.1-0.20220104203543-349281033d17
+require (
+	github.com/emadolsky/automaxprocs v1.4.1-0.20220104203543-349281033d17
+	go.uber.org/zap v1.21.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
