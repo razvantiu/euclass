a6e4670360cb7223b29549d9f04664db7713f92b
William Kennedy <bill@ardanlabs.com>
2022-02-09 10:03:41 -0500

    day3 push9

--- app/services/sales-api/handlers/testgrp/testgrp.go
+++ app/services/sales-api/handlers/testgrp/testgrp.go
@@ -7,13 +7,14 @@ import (
 	"math/rand"
 	"net/http"
 
+	"github.com/ardanlabs/service/business/web/trusted"
 	"github.com/ardanlabs/service/foundation/web"
 )
 
 func Handler(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	if n := rand.Int31n(100); n%2 == 0 {
-		//return trusted.NewRequestError(errors.New("TRUST ME"), http.StatusBadRequest)
-		return errors.New("DON'T TRUST ME")
+		return trusted.NewRequestError(errors.New("TRUST ME"), http.StatusBadRequest)
+		// return errors.New("DON'T TRUST ME")
 	}
 
 	status := struct {
