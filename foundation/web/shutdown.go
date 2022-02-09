4dc100e4e27b02c2b9c523040a50cdb667ed486b
William Kennedy <bill@ardanlabs.com>
2022-02-09 09:34:37 -0500

    day3 push4

--- foundation/web/shutdown.go
+++ foundation/web/shutdown.go
@@ -1,5 +1,7 @@
 package web
 
+import "errors"
+
 // shutdownError is a type used to help with the graceful termination of the service.
 type shutdownError struct {
 	Message string
@@ -14,4 +16,11 @@ func NewShutdownError(message string) error {
 // Error is the implementation of the error interface.
 func (se *shutdownError) Error() string {
 	return se.Message
+}
+
+// IsShutdown checks to see if the shutdown error is contained
+// in the specified error value.
+func IsShutdown(err error) bool {
+	var se *shutdownError
+	return errors.As(err, &se)
 }
