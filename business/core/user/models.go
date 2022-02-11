d9b0e02527e1c64f34a295bd2e5cbb4bcff697de
William Kennedy <bill@ardanlabs.com>
2022-02-11 10:35:42 -0500

    day5 push7

--- business/core/user/models.go
+++ business/core/user/models.go
@@ -27,6 +27,20 @@ type NewUser struct {
 	PasswordConfirm string   `json:"password_confirm" validate:"eqfield=Password"`
 }
 
+// UpdateUser defines what information may be provided to modify an existing
+// User. All fields are optional so clients can send just the fields they want
+// changed. It uses pointer fields so we can differentiate between a field that
+// was not provided and a field that was provided as explicitly blank. Normally
+// we do not want to use pointers to basic types but we make exceptions around
+// marshalling/unmarshalling.
+type UpdateUser struct {
+	Name            *string  `json:"name"`
+	Email           *string  `json:"email" validate:"omitempty,email"`
+	Roles           []string `json:"roles"`
+	Password        *string  `json:"password"`
+	PasswordConfirm *string  `json:"password_confirm" validate:"omitempty,eqfield=Password"`
+}
+
 // =============================================================================
 
 func toUser(dbUsr db.User) User {
