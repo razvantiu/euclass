95d7d60fd312fcd58601df00aac48c8f6a1c0176
William Kennedy <bill@ardanlabs.com>
2022-02-11 08:34:16 -0500

    day5 push2

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
@@ -11,17 +12,52 @@ import (
 	"os"
 	"time"
 
+	"github.com/ardanlabs/service/business/data/dbschema"
 	"github.com/ardanlabs/service/business/sys/auth"
+	"github.com/ardanlabs/service/business/sys/database"
 	"github.com/ardanlabs/service/foundation/keystore"
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := gentoken2()
+	err := migrate()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func migrate() error {
+	dbConfig := database.Config{
+		User:       "postgres",
+		Password:   "postgres",
+		Host:       "localhost",
+		Name:       "postgres",
+		DisableTLS: true,
+	}
+
+	db, err := database.Open(dbConfig)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := dbschema.Migrate(ctx, db); err != nil {
+		return fmt.Errorf("migrate database: %w", err)
+	}
+
+	fmt.Println("migrations complete")
+
+	if err := dbschema.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+
+	return nil
 }
 
 func gentoken2() error {
